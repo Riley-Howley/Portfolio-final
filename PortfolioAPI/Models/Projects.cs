5242ec8152a68be1d0d1fd0611504bf53a216f03
Riley Howley <71965268+Riley-Howley@users.noreply.github.com>
2022-04-23 23:01:11 +1200

    Bugs

--- PortfolioAPI/Models/Projects.cs
+++ PortfolioAPI/Models/Projects.cs
@@ -8,5 +8,7 @@ namespace Portfolio.Models
         public string BackgroundImage { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string GitHubLink { get; set; }
+        public string DeviceCompatable { get; set; }
     }
 }
